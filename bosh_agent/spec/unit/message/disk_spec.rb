f2e91d19493aec3c173f51ad8c38cc3a2f40a31f
Jeffrey Peckham <peckham@pivotallabs.com>
2013-10-01 15:58:19 -0700

    Move DiskUtil into own file and out of Message namespace
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotallabs.com>
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>
    
    Conflicts:
    	bosh_agent/spec/unit/message/disk_spec.rb

--- bosh_agent/spec/unit/message/disk_spec.rb
+++ bosh_agent/spec/unit/message/disk_spec.rb
@@ -111,10 +111,10 @@ describe Bosh::Agent::Message::UnmountDisk do
     platform = double(:platform)
     Bosh::Agent::Config.stub(:platform).and_return(platform)
     platform.stub(:lookup_disk_by_cid).and_return("/dev/sdy")
-    Bosh::Agent::Message::DiskUtil.stub(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
+    Bosh::Agent::DiskUtil.stub(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
 
     handler = Bosh::Agent::Message::UnmountDisk.new
-    Bosh::Agent::Message::DiskUtil.stub(:umount_guard)
+    Bosh::Agent::DiskUtil.stub(:umount_guard)
 
     handler.unmount(["4"]).should == { :message => "Unmounted /dev/sdy1 on /foomount"}
   end
@@ -123,7 +123,7 @@ describe Bosh::Agent::Message::UnmountDisk do
     platform = double(:platform)
     Bosh::Agent::Config.stub(:platform).and_return(platform)
     platform.stub(:lookup_disk_by_cid).and_return("/dev/sdx")
-    Bosh::Agent::Message::DiskUtil.stub(:mount_entry).and_return(nil)
+    Bosh::Agent::DiskUtil.stub(:mount_entry).and_return(nil)
 
     handler = Bosh::Agent::Message::UnmountDisk.new
     handler.stub(:umount_guard)
@@ -144,7 +144,7 @@ describe Bosh::Agent::Message::ListDisk do
     platform = double(:platform)
     Bosh::Agent::Config.stub(:platform).and_return(platform)
     platform.stub(:lookup_disk_by_cid).and_return("/dev/sdy")
-    Bosh::Agent::Message::DiskUtil.stub(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
+    Bosh::Agent::DiskUtil.stub(:mount_entry).and_return('/dev/sdy1 /foomount fstype')
 
     settings = { "disks" => { "persistent" => { 199 => 2 }}}
     Bosh::Agent::Config.settings = settings
@@ -154,58 +154,3 @@ describe Bosh::Agent::Message::ListDisk do
 
 end
 
-describe Bosh::Agent::Message::DiskUtil do
-  describe '#get_usage' do
-    it 'should return the disk usage' do
-      base = Bosh::Agent::Config.base_dir
-
-      fs_list = [
-          double('system', :dir_name => '/'),
-          double('ephermal', :dir_name => File.join(base, 'data')),
-          double('persistent', :dir_name => File.join(base, 'store'))
-      ]
-
-      sigar = double('sigar', :file_system_list => fs_list, :logger= => nil)
-
-      u1 = double('usage', :use_percent => 0.69)
-      sigar.should_receive(:file_system_usage).with('/').and_return(u1)
-
-      u2 = double('usage', :use_percent => 0.73)
-      sigar.should_receive(:file_system_usage).with(File.join(base, 'data')).and_return(u2)
-
-      u3 = double('usage', :use_percent => 0.11)
-      sigar.should_receive(:file_system_usage).with(File.join(base, 'store')).and_return(u3)
-
-      Sigar.stub(:new => sigar)
-
-      described_class.get_usage.should == {
-          :system => {:percent => '69'},
-          :ephemeral => {:percent => '73'},
-          :persistent => {:percent => '11'}
-      }
-    end
-
-    it 'should not return ephemeral and persistent disks usages if do not exist' do
-      base = Bosh::Agent::Config.base_dir
-
-      fs_list = [
-          double('system', :dir_name => '/'),
-      ]
-
-      sigar = double('sigar', :file_system_list => fs_list, :logger= => nil)
-
-      u1 = double('usage', :use_percent => 0.69)
-      sigar.should_receive(:file_system_usage).with('/').and_return(u1)
-
-      sigar.should_not_receive(:file_system_usage).with(File.join(base, 'data'))
-
-      sigar.should_not_receive(:file_system_usage).with(File.join(base, 'store'))
-
-      Sigar.stub(:new => sigar)
-
-      described_class.get_usage.should == {
-          :system => {:percent => '69'}
-      }
-    end
-  end
-end
