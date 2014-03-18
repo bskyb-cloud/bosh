2d986019add0e4b2aa63201285e6010ee563c0be
osmanj <julian.osman@bskyb.com>
2014-03-18 10:22:08 +0000

    removed configuration from munin-node artifacts

--- stemcell_builder/stages/munin_node/apply.sh
+++ stemcell_builder/stages/munin_node/apply.sh
@@ -1,33 +1,29 @@
 #!/usr/bin/env bash
-
 #
-
-# Copyright (c) 2009-2012 VMware, Inc.
-
-
 
 set -e
 
-
-
 base_dir=$(readlink -nf $(dirname $0)/../..)
-
 source $base_dir/lib/prelude_apply.bash
 
 
 
 # Disable interactive dpkg
-
 debconf="debconf debconf/frontend select noninteractive"
-
 run_in_chroot $chroot "echo ${debconf} | debconf-set-selections"
 
-
-
 # Install base debs needed by both the warden and bosh
-
 debs="munin-node"
 
 
-
 pkg_mgr install $debs
+
+# now make the directory with the correct permissions
+run_in_chroot $chroot mkdir -p /var/run/munin
+run_in_chroot $chroot chown root:root /var/run/munin
+run_in_chroot $chroot chmod 644 /var/run/munin
+
+# remove all of the currently linked in munin plugins
+run_in_chroot $chroot rm /etc/munin/plugins/*
+run_in_chroot $chroot rm /etc/munin/plugin-conf.d/*
+run_in_chroot $chroot rm /etc/munin/munin-node.conf
