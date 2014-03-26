15e8fe7771a767a57c8862b2171e886699a29d49
osmanj <julian.osman@bskyb.com>
2014-03-26 14:18:26 +0000

    fixed commands

--- stemcell_builder/stages/munin_node/apply.sh
+++ stemcell_builder/stages/munin_node/apply.sh
@@ -19,11 +19,11 @@ debs="munin-node"
 pkg_mgr install $debs
 
 # now make the directory with the correct permissions
-run_in_chroot $chroot mkdir -p /var/run/munin
-run_in_chroot $chroot chown root:root /var/run/munin
-run_in_chroot $chroot chmod 644 /var/run/munin
+run_in_chroot $chroot "mkdir -p /var/run/munin"
+run_in_chroot $chroot "chown root:root /var/run/munin"
+run_in_chroot $chroot "chmod 644 /var/run/munin"
 
 # remove all of the currently linked in munin plugins
-run_in_chroot $chroot rm /etc/munin/plugins/*
-run_in_chroot $chroot rm /etc/munin/plugin-conf.d/*
-run_in_chroot $chroot rm /etc/munin/munin-node.conf
+run_in_chroot $chroot "rm /etc/munin/plugins/*"
+run_in_chroot $chroot "rm /etc/munin/plugin-conf.d/*"
+run_in_chroot $chroot "rm /etc/munin/munin-node.conf"
